4c80dcf3ddef46896361f99d3ebb349496e52c92
pierrepapot <pierrepapot@sfr.mail>
2018-01-25 14:11:39 +0100

    commit final exo4

---
+++ project/src/components/weather/weather.controller.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	function WeatherCtrl(){
+		var vm = this;
+	}
+
+	WeatherCtrl.$inject = [];
+
+	angular.module('daproject').controller('WeatherCtrl', WeatherCtrl);
+})();
